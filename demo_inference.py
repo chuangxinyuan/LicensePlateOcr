69ef38bdb80cc5b9164201af4b3e0d9e574d12a2
cuiyl <cuiyl@shaiic.com>
2022-07-25 15:09:17 +0800

    update ocr pre-trained model and training script

--- demo_inference.py
+++ demo_inference.py
@@ -15,115 +15,133 @@ python demo_inference.py --batch_size=32 \
   --checkpoint=model.ckpt-399731\
   --image_path_pattern=./datasets/data/fsns/temp/fsns_train_%02d.png
 """
+import argparse
+from LPRNet import build_lprnet
+
+from PIL import Image,ImageDraw,ImageFont
+
 import numpy as np
-import PIL.Image
-import tensorflow as tf
-from tensorflow.python.platform import flags
-from tensorflow.python.training import monitored_session
+
 import cv2
 import os
-import common_flags
-import re
-import datasets
+
 import ast
-import data_provider
-import glob
-FLAGS = flags.FLAGS
-common_flags.define()
-
-# e.g. ./datasets/data/fsns/temp/fsns_train_%02d.png
-flags.DEFINE_string('image_path_pattern', '',
-                    'A file pattern with a placeholder for the image index.')
-flags.DEFINE_string('license_boxes_json_path', '/data/output.json', 'A json file with coordinates of bounding boxes for license plates')
+import torch
 
 
-def get_dataset_image_size(dataset_name):
-  # Ideally this info should be exposed through the dataset interface itself.
-  # But currently it is not available by other means.
-  ds_module = getattr(datasets, dataset_name)
-  height, width, _ = ds_module.DEFAULT_CONFIG['image_shape']
-  return width, height
+
+CHARS = ['京', '沪', '津', '渝', '冀', '晋', '蒙', '辽', '吉', '黑',
+         '苏', '浙', '皖', '闽', '赣', '鲁', '豫', '鄂', '湘', '粤',
+         '桂', '琼', '川', '贵', '云', '藏', '陕', '甘', '青', '宁',
+         '新',
+         '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K',
+         'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+         'W', 'X', 'Y', 'Z', 'I', 'O', '-'
+         ]
+
+CHARS_DICT = {i:char for i, char in enumerate(CHARS)}
 
 
-def load_images(batch_size, dataset_name, annotations):
-  width, height = get_dataset_image_size(dataset_name)
-  images_actual_data = np.ndarray(shape=(len(annotations.values()), height, width, 3),
-                                  dtype='uint8')
-  count = 0
+
+
+
+def create_model(args):
+    lprnet = build_lprnet(lpr_max_len=args.lpr_max_len, phase=args.phase_train, class_num=len(CHARS), dropout_rate=args.dropout_rate)
+    device = torch.device("cuda:0" if args.cuda else "cpu")
+    lprnet.to(device)
+    print("Successful to build network!")
+
+    # load pretrained model
+    if args.pretrained_model:
+        lprnet.load_state_dict(torch.load(args.pretrained_model, map_location=torch.device('cpu')))
+        print("load pretrained model successful!")
+    else:
+        print("[Error] Can't found pretrained mode, please check!")
+        return False
+    return lprnet 
+
+def run(args, annotations):
+  model = create_model(args)
+
+
   for path,boxes in annotations.items():
-        print("Processing: ", path) 
-        img = cv2.imread(os.path.join('/mnt/data/datasets/images', os.path.basename(path)))
-        for box in boxes:
-            print(box)
-            print(img.shape)
-            img_cropped = img[box['xmin']:box['xmax']+1, box['ymin']:box['ymax']+1]
-            cv2.imwrite('/data/{}.jpg'.format(count), img_cropped)
-            pil_img = PIL.Image.fromarray(img_cropped)
-            img = pil_img.resize((width, height), PIL.Image.ANTIALIAS)
-            images_actual_data[count, ...] = np.asarray(img)
-            count += 1
-  return images_actual_data
+      print("Processing: ", path) 
+      img = cv2.imread(os.path.join('/mnt/data/datasets/images', os.path.basename(path)))
+      for box in boxes:
+          img_cropped = img[box['ymin']:box['ymax']+1, box['xmin']:box['xmax']+1]
+          height, width, _ = img_cropped.shape
+          if height != args.img_size[1] or width != args.img_size[0]:
+              img_cropped = cv2.resize(img_cropped, args.img_size)
+
+          img_cropped = img_cropped.astype('float32')
+          img_cropped -= 127.5
+          img_cropped *= 0.0078125
+          img_cropped = np.expand_dims(np.transpose(img_cropped, (2, 0, 1)), axis=0) 
 
 
-def create_model(batch_size, dataset_name):
-  width, height = get_dataset_image_size(dataset_name)
-  dataset = common_flags.create_dataset(split_name=FLAGS.split_name)
-  model = common_flags.create_model(
-      num_char_classes=dataset.num_char_classes,
-      seq_length=dataset.max_sequence_length,
-      num_views=dataset.num_of_views,
-      null_code=dataset.null_code,
-      charset=dataset.charset)
-  raw_images = tf.compat.v1.placeholder(
-      tf.uint8, shape=[batch_size, height, width, 3])
-  images = tf.map_fn(data_provider.preprocess_image, raw_images,
-                     dtype=tf.float32)
-  endpoints = model.create_base(images, labels_one_hot=None)
-  return raw_images, endpoints
+          prebs = model(torch.tensor(img_cropped))
+          # greedy decode
+          prebs = prebs.cpu().detach().numpy()
+          preb_labels = list()
+          for i in range(prebs.shape[0]):
+              preb = prebs[i, :, :]
+              preb_label = list()
+              for j in range(preb.shape[1]):
+                  preb_label.append(np.argmax(preb[:, j], axis=0))
+              no_repeat_blank_label = list()
+              pre_c = preb_label[0]
+              if pre_c != len(CHARS) - 1:
+                  no_repeat_blank_label.append(pre_c)
+              for c in preb_label: # dropout repeate label and blank label
+                  if (pre_c == c) or (c == len(CHARS) - 1):
+                      if c == len(CHARS) - 1:
+                          pre_c = c
+                      continue
+                  no_repeat_blank_label.append(c)
+                  pre_c = c
+              preb_labels.append(no_repeat_blank_label)
+
+          output = ''.join([str(CHARS_DICT[i.item()]) for i in preb_labels[0]])
 
 
-def run(checkpoint, batch_size, dataset_name, image_path_pattern, annotations):
-  images_placeholder, endpoints = create_model(batch_size,
-                                               dataset_name)
-  session_creator = monitored_session.ChiefSessionCreator(
-      checkpoint_filename_with_path=checkpoint)
-  count = 0
-  width, height = get_dataset_image_size(dataset_name)
-  with monitored_session.MonitoredSession(
-          session_creator=session_creator) as sess:
-    for path,boxes in annotations.items():
-        print("Processing: ", path) 
-        img = cv2.imread(os.path.join('/mnt/data/datasets/images', os.path.basename(path)))
-        for box in boxes:
-            img_cropped = img[box['ymin']:box['ymax']+1, box['xmin']:box['xmax']+1]
-            pil_img = PIL.Image.fromarray(img_cropped)
-            pil_img_cropped = pil_img.resize((width, height), PIL.Image.ANTIALIAS)
-            count += 1
-            predictions = sess.run(endpoints.predicted_text,
-                           feed_dict={images_placeholder: np.asarray(pil_img_cropped)[np.newaxis, ...]})
-            output = [pr_bytes.decode('utf-8') for pr_bytes in predictions.tolist()][0]
-            output = re.sub(r'([^\s\w]|_)+', '', output)
-            cv2.rectangle(img,(box['xmin'],box['ymin']),(box['xmax'],box['ymax']),(0,255,0),3)
-            cv2.putText(img, output.replace('?',''), (box['xmin'],box['ymin']-10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+          cv2.rectangle(img,(box['xmin'],box['ymin']),(box['xmax'],box['ymax']),(0,255,0),3)
+          font = ImageFont.truetype('yahei_mono_0.ttf',60)
+          img_pil = Image.fromarray(cv2.cvtColor(img,cv2.COLOR_BGR2RGB))
+          draw = ImageDraw.Draw(img_pil)
+          draw.text((box['xmin'],box['ymin']-10),output.replace('?',''),font=font,fill=(0,255,0)) 
+          img_ocv = np.array(img_pil)                    
+          img = cv2.cvtColor(img_ocv,cv2.COLOR_RGB2BGR)
 #             file_writer.write([pr_bytes.decode('utf-8') for pr_bytes in predictions.tolist()][0])
-        cv2.imwrite('/mnt/output/'+os.path.basename(path), img)
+      cv2.imwrite('/mnt/output/'+os.path.basename(path), img)
   
 
+def get_parser():
+    parser = argparse.ArgumentParser(description='parameters to train net')
+    parser.add_argument('--license_boxes_json_path', default='/mnt/data/outputdata/output.json', help='detection output ')
+    parser.add_argument('--batch_size', default=1, help='the batch size')
 
-def main(_):
-  data = open(FLAGS.license_boxes_json_path, 'r')
+    parser.add_argument('--img_size', default=(94, 24), help='the image size')
+    parser.add_argument('--dropout_rate', default=0, help='dropout rate.')
+    parser.add_argument('--lpr_max_len', default=8, help='license plate number max length.')
+    parser.add_argument('--phase_train', default=False, type=bool, help='train or test phase flag.')
+    parser.add_argument('--cuda', default=False, type=bool, help='Use cuda to train model')
+    parser.add_argument('--pretrained_model', default='./lpr_model.pth', help='pretrained base model')
+
+    args = parser.parse_args()
+
+    return args
+
+def main():
+  args = get_parser()
+  data = open(args.license_boxes_json_path, 'r')
   annotations = ast.literal_eval(data.read())['annotations']
-  FLAGS.batch_size = 1
+
   if not os.path.exists('/mnt/output/'):
       os.makedirs('/mnt/output/')
-  d = open(os.path.join(FLAGS.checkpoint, 'checkpoint'), 'r')
-  for line in d.readlines():
-    if not 'all_model_checkpoint_path' in line:
-        checkpoint = os.path.basename(line.split(':')[1].strip().strip('"'))
-        break
-  run(os.path.join(FLAGS.checkpoint, checkpoint), FLAGS.batch_size, FLAGS.dataset_name,
-                    FLAGS.image_path_pattern, annotations)
+
+  run(args, annotations)
 
 
 if __name__ == '__main__':
-  tf.compat.v1.app.run()
+  main()
